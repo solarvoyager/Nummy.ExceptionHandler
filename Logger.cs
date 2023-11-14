8920f1da6bf81743b6fcf5d3440a71e1bb2d360e
Hamid Musayev <hemidvsmusayev@gmail.com>
2023-11-14 16:17:17 +0400

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,10 @@
+﻿namespace Nummy.Exception
+{
+        public class Logger
+        {
+            public void Log(string text)
+            {
+                Console.WriteLine(text);
+            }
+        }
+}
