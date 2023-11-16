593944a760f3707370347686a81c1f1fd834d9e3
Hamid Musayev <hemidvsmusayev@gmail.com>
2023-11-16 12:03:16 +0400

    model props

--- Extensions/NummyExceptionServiceExtension.cs
+++ Extensions/NummyExceptionServiceExtension.cs
@@ -2,18 +2,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using Nummy.ExceptionHandler.Middlewares;
 using Nummy.ExceptionHandler.Models;
-using Nummy.ExceptionHandler.Service;
 
 namespace Nummy.ExceptionHandler.DependencyInjection
 {
     public static class NummyExceptionServiceExtension
     {
-        public static void AddNummyExceptionHander(this IServiceCollection services, Action<NummyExceptionOptions>? options = null)
+        public static void AddNummyExceptionHander(this IServiceCollection services, Action<NummyExceptionOptions> options)
         {
-            services.AddScoped<ILogger, Logger>();
-
-            if (options is not null)
-                services.Configure(options);
+            services.Configure(options);
         }
 
         public static void UsNummyExceptionHandler(IApplicationBuilder app)
